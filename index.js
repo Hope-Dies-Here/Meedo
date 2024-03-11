2a23d41b5fa5e2d3f4c1ba95bb60c8714277427b
Dummy <onyourleftpal@gmail.com>
2024-03-11 13:38:34 +0300

    v2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("this is type script test");
+var $ = function (element) {
+    return document.querySelector(element);
+};
+$("#meedo-home-text").addEventListener("onhover", function () {
+    alert("ooooo");
+});
+$("#meedo-home-text").classList.add("add-red");
+setTimeout(function () {
+    $("#meedo-home-text").classList.add("add-opacity");
+    $("#dribbble").style = "opacity: .6";
+}, 22000);
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-alert("this is type script test")
+const $ = (element) => {
+  return document.querySelector(element)
+}
+$("#meedo-home-text").addEventListener("onhover", () => {
+  alert("ooooo")
+})
+
+$("#meedo-home-text").classList.add("add-red")
+setTimeout(function() {
+  $("#meedo-home-text").classList.add("add-opacity")
+  $("#dribbble").style = "opacity: .6"
+}, 22000);
