caaa36f98f555cee740266e4d485e88ad06f984a
Dummy <onyourleftpal@gmail.com>
2024-03-14 09:37:34 +0300

    added artists page

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var $ = function (element) {
     return document.querySelector(element);
 };
-$("#meedo-home-text").addEventListener("onhover", function () {
-    alert("ooooo");
-});
-$("#meedo-home-text").classList.add("add-red");
-setTimeout(function () {
-    $("#meedo-home-text").classList.add("add-opacity");
-    $("#dribbble").style = "opacity: .6";
-}, 22000);
+var a = $("#balance");
+var letters = a.innerText;
+letters[0].style = "fontsize: 1rem";
+letters[1].style = "fontsize: 1.618rem";
+letters[2].style = "fontsize: 2.618rem";
+letters[3].style = "fontsize: 4.236rem";
+letters[4].style = "fontsize: 2.618rem";
+letters[5].style = "fontsize: 1.618rem";
+letters[5].style = "fontsize: 1.618rem";
+letters[6].style = "font-size: 1rem!important";
